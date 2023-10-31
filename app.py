19ecfb0b0bddc87a868493de133edd65a1d75b93
mzmarci <mzmazy100@gmail.com>
2023-10-31 05:52:57 +0100

    updating

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 version = "1.7.1"
 
 user = os.environ.get("POSTGRES_USER")
-pw = os.environ.get("POSTGRES_PASSWORD")
+#pw = os.environ.get("POSTGRES_PASSWORD")
+pw =marci123
 host = os.environ.get("POSTGRES_HOST")
 db = os.environ.get("POSTGRES_DATABASE_NAME")
 DB_URL = f"postgresql+psycopg2://{user}:{pw}@{host}/{db}"
